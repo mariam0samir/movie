1bddbb28f44ab905b9ef359ec20b59b002bd4dc9
mariam0samir <131833351+mariam0samir@users.noreply.github.com>
2025-08-09 00:17:31 +0300

    Fix routing for GitHub Pages using HashRouter

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import { Container } from "react-bootstrap";
-import  {contactInfo }  from '../../Data/ContactInfo'
+import { contactInfo } from '../../Data/ContactInfo'
 import './Contact.css'
 
 export default function Contact() {
@@ -11,11 +11,14 @@ export default function Contact() {
       <Container>
         <div className="content py-5">
           <div className="row g-4 justify-content-center ">
-            {contactInfo.map(({name, icon }) => (
-              <div className="col-lg-4 col-md-6 col-sm-12">
-                <a target="_blank"
-                   href="https://mail.google.com/mail/?view=cm&fs=1&to=momopasha09090@gmail.com"
-                   className="bg-blue d-flex justify-content-center align-items-center flex-column rounded">
+            {contactInfo.map(({ name, icon }) => (
+              <div className="col-lg-4 col-md-6 col-sm-12" key={name}>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://mail.google.com/mail/?view=cm&fs=1&to=momopasha09090@gmail.com"
+                  className="bg-blue d-flex justify-content-center align-items-center flex-column rounded"
+                >
                   {icon}
                   <h5 className="text-white mb-5">{name}</h5>
                 </a>
@@ -27,5 +30,3 @@ export default function Contact() {
     </div>
   );
 }
-
-
